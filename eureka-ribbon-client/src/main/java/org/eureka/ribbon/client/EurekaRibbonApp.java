e35da6a9792cce3dd83990c14d2a47255f860a60
xiaogao <puremoonblingbling@163.com>
2019-08-20 23:07:17 +0800

    增加在RestTemplate和Ribbon上使用熔断器功能

--- eureka-ribbon-client/src/main/java/org/eureka/ribbon/client/EurekaRibbonApp.java
+++ eureka-ribbon-client/src/main/java/org/eureka/ribbon/client/EurekaRibbonApp.java
@@ -3,6 +3,7 @@ package org.eureka.ribbon.client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 /**
  * Ribbon负载均衡启动类
@@ -11,6 +12,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableHystrix // 启动熔断器功能
 public class EurekaRibbonApp 
 {
     public static void main( String[] args )
--- eureka-ribbon-client/src/main/java/org/eureka/ribbon/client/service/RibbonService.java
+++ eureka-ribbon-client/src/main/java/org/eureka/ribbon/client/service/RibbonService.java
@@ -7,11 +7,24 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+/**
+ * 负载均衡实现服务类
+ * @author gaomeiling
+ * @date 2019年8月20日
+ *
+ */
 @Service
 public class RibbonService {
 
 	@Autowired
 	RestTemplate restTemplate;
+	/*
+	 * 使hi()启用Hystrix熔断器的功能
+	 * fallbackMethod为处理回退逻辑的方法
+	 */
+	@HystrixCommand(fallbackMethod="hiError") 
 	public String hi(String name){
 		return restTemplate.getForObject("http://eureka-client/hi?name="+name, String.class);
 	}
@@ -26,5 +39,18 @@ public class RibbonService {
 		// 选择一个eureka-client的服务实例信息
 		ServiceInstance instance = loadBalancer.choose("eureka-client"); 
 		return instance.getHost()+":"+instance.getPort();
+	}
+	
+	/**
+	 * 处理回退逻辑的方法
+	 * 在熔断器打开的情况下执行
+	 * 回退逻辑最好是返回一些静态字符串，不需要处理复杂的逻辑，也不需要远程调度其它服务，这样方便执行快速失败，释放线程资源
+	 * @param name
+	 * @return
+	 * @author gaomeiling
+	 * @date 2019年8月20日
+	 */
+	public String HiError(String name){
+		return "hi,"+name+",sorry,error!";
 	}
 }
